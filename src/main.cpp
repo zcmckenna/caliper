835d976c9e516f7f2486b8f0c4165c29c84b4ffa
Zach McKenna <zmckenna321@gmail.com>
2019-11-26 20:14:11 -0700

    Added main menu options

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,52 @@
-#include <cstdlib>
-#include <iostream>
+/* Caliper
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ */
 
-int main () {
-    std::cout << "Welcome to Caliper!";
-    std::cout << "Would you like to manage (1) Vehicles or (2) Maintenance Records: ";
-    std::int userInput;
-    std::cin >> userInput;
+#include <iostream> // For cin and cout
+#include <fstream> // For reading and writing to and from files
+#include <limits> // For INT_MAX
+using namespace std;
 
-
-    return 0;
+// Must have a function named "main", which is the starting point of a C++ program.
+int main() {
+    ifstream welcomeInput("../welcome-text");
+    
+    if(welcomeInput.fail()){
+        cerr << "Failed opening welcome text file";
+        return -1;
+    } // Error message if can't find welcome text file
+    cout << endl;
+    for(string welcomeLine; getline(welcomeInput, welcomeLine);){
+        cout << welcomeLine << endl;
+    } // Open welcome-text file from the root directory and output the parse the text 
+    int mainMenuInput;
+    
+    while(true){
+        mainMenuInput = 0;
+        cout << endl
+             << "(1) List maintenance history"
+             << endl
+             << "(2) Manage maintenance records"
+             << endl
+             << ":";
+        
+        if(!(cin >> mainMenuInput)){
+            cout << "Please enter an integer..." << endl;
+            cin.clear();
+		    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+        }else if(mainMenuInput != 1 && mainMenuInput != 2){
+            cout << "Please enter either 1 or 2..." << endl;
+        }else{
+            break;
+        }
+    }
+    cout << "Good to go..." << endl;
+	return 0;
 }
